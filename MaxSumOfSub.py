ce9b5b5febbb5626c8ca58b33365a36da0a0ae11
buyibuyi001 <361143840@qq.com>
2018-05-19 16:20:01 +0800

    Update MaxSumOfSub.py

--- MaxSumOfSub.py
+++ MaxSumOfSub.py
@@ -1,24 +1,16 @@
 def maxSumOfSubarray(alist):
-
     if alist==[]:
         return 0
     alist_len=len(alist)
     curMax=finMax=curSum=alist[0]
 
-    for i in range(1,alist_len):
-        
-            
+    for i in range(1,alist_len):        
         curMax=max(alist[i],max(curSum,curSum+alist[i]))
         finMax=max(finMax,curMax )
-
         if curSum+alist[i]<0:
             curSum=0
         else:
             curSum+=alist[i]
-        
-
-        
-
     return finMax
 
 alist=[-111,-2,-3,0,1,4,-1,9]
